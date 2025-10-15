51908f2c560d18e3a37ee4653f7e1e06d66849b0
krishna sahu <krishna.sahu.work@gmail.com>
2025-10-16 01:05:00 +0530

    Login logic done in usercontroller

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -50,7 +50,61 @@ try{
 }
 
 }
+export async function UserLogIn(req: Request, res: Response) {
 
+const requiredbody = z.object({
+    email: z.string().email().min(3).max(50),
+    password: z.string().min(8).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/,"Password must include uppercase, lowercase, number, and special character")
+})
+
+const parseddata = requiredbody.safeParse(req.body)
+
+if(!parseddata.success){
+    return res.status(400).json({
+        success:false,
+        message:"email and password are required"
+    })
+}
+const {email,password}= parseddata.data
+try{
+    const user = await UserModel.findOne({email})
+    if(!user){
+        return res.status(403).json({
+            success:false,
+            message:"user does not exists , please signup first"
+        })
+    }
+
+const ok = await bcrypt.compare(password,user.password)
+if(!ok){
+    return res.status(403).json({
+        success:false,
+        message:"wrong password"
+    })
+}
+ const token = jwt.sign({id:user._id},JWT_SECRET, {expiresIn:"7d"})
+
+ 
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+        return res.status(200).json({
+      success: true,
+      message: "Signin successful",
+      user: { id: user._id, username: user.username, email: user.email },
+    
+    });
+
+}catch(error){
+    console.error("Signin error:", error);
+    return res.status(500).json({ success: false, message: "Server error" });
+}
+
+}
 export async function UserLogout(req: Request, res: Response) {
     res.send("logout endpoint ")
 }
