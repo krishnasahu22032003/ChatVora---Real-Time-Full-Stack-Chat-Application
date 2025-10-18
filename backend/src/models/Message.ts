d147de695768b285cbf10a97c7e263ca85d44523
krishna sahu <krishna.sahu.work@gmail.com>
2025-10-18 15:17:42 +0530

    Message schema done

---
+++ backend/src/models/Message.ts
@@ -0,0 +1,25 @@
+import mongoose, { Types } from "mongoose";
+
+
+interface MessagesTypes {
+
+senderId:Types.ObjectId
+receiverId:Types.ObjectId
+text:string,
+image:string
+
+
+}
+
+const UserMessageSchema = new mongoose.Schema<MessagesTypes>({
+
+senderId:{type:mongoose.Schema.Types.ObjectId,ref:"user",required:true},
+receiverId:{type:mongoose.Schema.Types.ObjectId,ref:"user",required:true},
+text:{type:String},
+image:{type:String },
+
+},{timestamps:true})
+
+ const UserMessage = mongoose.model("UserMessage",UserMessageSchema) 
+
+ export default UserMessage
