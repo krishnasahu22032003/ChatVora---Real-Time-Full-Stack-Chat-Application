9862d4b9edafef508bce34b65de5eda70441b2d3
krishna sahu <krishna.sahu.work@gmail.com>
2025-10-19 23:50:44 +0530

    getchatpartners functions added to get the chatting partners

--- backend/src/controllers/MessageController.ts
+++ backend/src/controllers/MessageController.ts
@@ -2,7 +2,7 @@
 import type { Request, Response } from "express";
 import { UserModel } from "../models/UserModel.js";
 import mongoose from "mongoose";
-import z, { success }  from "zod";
+import z from "zod";
 import UserMessage from "../models/Message.js";
 import cloudinary from "../lib/cloudinary.js";
 
@@ -13,7 +13,7 @@ interface GetAllContactsBody {
 
 const idSchema = z.string().refine(
   (val) => mongoose.Types.ObjectId.isValid(val),
-{message:"Invalid user id format "}
+  { message: "Invalid user id format " }
 )
 
 export const getAllContacts = async (
@@ -21,7 +21,7 @@ export const getAllContacts = async (
   res: Response
 ) => {
   try {
-   
+
     const loggedInUserId = req.user?._id;
     const filteredUsers = await UserModel.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
@@ -32,69 +32,91 @@ export const getAllContacts = async (
   }
 };
 
-export const getMessagesByUserId = async (req:Request,res:Response)=>{
+export const getMessagesByUserId = async (req: Request, res: Response) => {
 
-try{
+  try {
 
-const myId =  req.user._id
+    const myId = req.user._id
 
-const {id:userToChatid} = req.params
+    const { id: userToChatid } = req.params
 
-const parsed  =  idSchema.safeParse(userToChatid)
+    const parsed = idSchema.safeParse(userToChatid)
 
-   if (!parsed.success) {
+    if (!parsed.success) {
       return res.status(400).json({
         success: false,
-       message: parsed.error?.issues?.[0]?.message || "Invalid user id",
+        message: parsed.error?.issues?.[0]?.message || "Invalid user id",
       });
     }
 
-const ValidId = parsed.data
+    const ValidId = parsed.data
 
-const Messages = await UserMessage.find({
-  $or:[{senderId:myId , receiverId: ValidId},
-    {senderId:ValidId , receiverId:myId}
-  ]
-})
-res.status(200).json(Messages)
+    const Messages = await UserMessage.find({
+      $or: [{ senderId: myId, receiverId: ValidId },
+      { senderId: ValidId, receiverId: myId }
+      ]
+    })
+    res.status(200).json(Messages)
 
-}catch(error){
-console.log("Error while Fetching messages",(error as Error).message)
-res.status(500).json({success:false,message:"Internal Server Error"})
+  } catch (error) {
+    console.log("Error while Fetching messages", (error as Error).message)
+    res.status(500).json({ success: false, message: "Internal Server Error" })
+  }
 }
-}
 
-export const SendMessages = async (req:Request,res:Response)=>{
+export const SendMessages = async (req: Request, res: Response) => {
 
-try{
+  try {
 
-  const {text,image} = req.body
-  const {id:receiverId} = req.params
-  const senderId =req.user._id
+    const { text, image } = req.body
+    const { id: receiverId } = req.params
+    const senderId = req.user._id
 
-  let imageUrl ;
+    let imageUrl;
 
-  if(image){
-    const uploadResponse = await cloudinary.uploader.upload(image)
-    imageUrl = uploadResponse.secure_url
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image)
+      imageUrl = uploadResponse.secure_url
+
+    }
+
+    const newMessage = new UserMessage({
+
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl
+
+    })
+    await newMessage.save()
+
+    res.status(200).json(newMessage)
+  } catch (error) {
+    console.log("Error in sending Messages", (error as Error).message)
+    res.status(500).json({ success: false, message: "Internal Server Error" })
 
   }
 
-  const newMessage = new UserMessage({
-
-senderId,
-receiverId,
-text,
-image:imageUrl
-
-  })
-await newMessage.save()
-
-res.status(200).json(newMessage)
-}catch(error){
-console.log("Error in sending Messages",(error as Error).message)
-res.status(500).json({success:false,message:"Internal Server Error"})
-
 }
+
+
+export const getChatPartner = async (req: Request, res: Response) => {
+
+  try {
+    const loggedInUserId = req.user._id
+
+    const messages = await UserMessage.find({
+
+      $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }]
+
+    })
+
+    const ChatPartnersId = [...new Set(messages.map((msg) => msg.senderId.toString() === loggedInUserId.toString() ? msg.receiverId.toString() : msg.senderId.toString()))]
+    const chatPartners = await UserModel.find({_id:{$in:ChatPartnersId}}).select("-password")
+res.status(200).json(chatPartners)
+
+  } catch (error) {
+
+  }
 
 }
--- backend/src/routes/MessageRoutes.ts
+++ backend/src/routes/MessageRoutes.ts
@@ -3,12 +3,16 @@ import { getAllContacts } from "../controllers/MessageController.js"
 import { AuthRoute } from "../middlewares/AuthMiddleware.js"
 import { getMessagesByUserId } from "../controllers/MessageController.js"
 import { SendMessages } from "../controllers/MessageController.js"
+import { getChatPartner } from "../controllers/MessageController.js"
+import { ArcjetProtection } from "../middlewares/ArcjetMiddleware.js"
 const messageRouter = express.Router()
 
-messageRouter.get("/contacts",AuthRoute,getAllContacts)
-// messageRouter.get("/chats",getChatPartner)
-messageRouter.get("/:id",AuthRoute,getMessagesByUserId)
-messageRouter.post("/send/:id",AuthRoute,SendMessages)
+
+ messageRouter.use(ArcjetProtection,AuthRoute)
+messageRouter.get("/contacts",getAllContacts)
+messageRouter.get("/chats",getChatPartner)
+messageRouter.get("/:id",getMessagesByUserId)
+messageRouter.post("/send/:id",SendMessages)
 
 
 export default messageRouter
