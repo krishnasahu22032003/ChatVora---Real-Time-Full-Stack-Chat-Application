dddcf3d16f93d2c7c7c6ece9789e2d87a77dc5ba
krishna sahu <krishna.sahu.work@gmail.com>
2025-11-03 21:17:46 +0530

    Activetabswitch component created

--- frontend/src/components/ActiveTabSwitch.tsx
+++ frontend/src/components/ActiveTabSwitch.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { useChatStore } from "../store/useChatStore";
 
-const ActiveTabSwitch = () => {
+function ActiveTabSwitch() {
+  const { activeTab, setActiveTab } = useChatStore();
+
   return (
-    <div>
-      
+    <div className="flex items-center justify-center gap-3 p-2">
+      {["chats", "contacts"].map((tab) => (
+        <button
+          key={tab}
+          onClick={() => setActiveTab(tab)}
+          className={`px-5 py-2 rounded-xl font-medium capitalize transition-all duration-300
+            ${
+              activeTab === tab
+                ? "bg-cyan-500/20 text-cyan-400 shadow-[0_0_10px_rgba(34,211,238,0.4)]"
+                : "text-slate-400 hover:text-cyan-300 hover:bg-cyan-400/10"
+            }`}
+        >
+          {tab}
+        </button>
+      ))}
     </div>
-  )
+  );
 }
 
-export default ActiveTabSwitch
+export default ActiveTabSwitch;
