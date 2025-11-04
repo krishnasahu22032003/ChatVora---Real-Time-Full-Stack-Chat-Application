206ce912731abcc157ef30dcb6c9af60009cb081
krishna sahu <krishna.sahu.work@gmail.com>
2025-11-04 19:38:31 +0530

    nochathistoryplaceholder created

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -2,6 +2,7 @@ import { useChatStore } from "../store/useChatStore"
 import { useAuthStore } from "../store/useAuthStore"
 import { useEffect } from "react"
 import ChatHeader from "./ChatHeader"
+import NoChatHistoryPlaceHolder from "./NoChatHistoryPlaceHolder"
 const ChatContainer = () => {
 
 const{selectedUser,getMessagesByUserId,messages}=useChatStore()
@@ -12,6 +13,10 @@ useEffect(()=>{
   return (
   <>
   <ChatHeader/>
+  <div className="flex-1 px-6 overflow-y-auto py-8">
+    {messages.length>0?(<p>some messages</p>):(<NoChatHistoryPlaceHolder name={selectedUser.username}/>)}
+
+  </div>
   </>
   )
 }
